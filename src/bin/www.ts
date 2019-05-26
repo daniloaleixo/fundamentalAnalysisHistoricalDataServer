ac0ec4ef258f4c598b7d82937671da57ca2b9d5d
Danilo Aleixo <danilo_aleixo@hotmail.com>
2019-05-26 18:55:34 -0300

    Fiz um inicial de um server em node
    - Tambem instalei o GraphQL e vou usá-lo em seguida

---
+++ src/bin/www.ts
@@ -0,0 +1,6 @@
+import server from '../server/server';
+
+
+server.listen(3000, () => {
+  console.log(`[SERVER] Running at http://localhost:3000`);
+});
---
+++ src/server/server.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+
+const server = express();
+server.get("/", (_, res) => {
+  res.send("Hello ts-node!");
+});
+export default server;
