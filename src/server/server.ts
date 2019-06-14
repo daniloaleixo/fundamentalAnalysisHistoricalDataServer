b9a98f6a8db98ebdc0a25248916397c0ebcdbcec
Danilo Aleixo <danilo_aleixo@hotmail.com>
2019-06-14 20:18:24 -0300

    Adding Apollo

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,43 @@
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+
 import express from "express";
+import { buildSchema } from 'graphql';
 
 
-const server: express.Express = express();
+const app: express.Express = express();
 
-server.get("/", (_, res) => {
+app.get("/", (_, res) => {
   res.send("Hello ts-node!");
 });
 
-export default server;
+
+
+
+
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+// Root resolver
+var root = {
+    message: () => 'Hello World!'
+};
+
+// app.use('/graphql', express_graphql({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true
+// }));
+
+app.use('/graphql', graphqlExpress((req: any) => ({
+  schema,
+  rootValue: root,
+  // context: {user: req.user && db.users.get(req.user.sub)}
+  graphiql: true
+})));
+// app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+
+export default app;
